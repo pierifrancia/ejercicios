a39f851f00d441d011f91d3c5a0c952f732f6c95
pierifrancia  <pieri.francia@gmail.com>
2019-10-21 20:12:14 -0300

    ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,17 @@
+let ingresos = 1000;
+
+let ganancias = 600;
+
+let impuestos = 500;
+
+let objetivoIngresos = 800;
+
+let objetivoGanancias = 600;
+
+let objetivoImpuestos = 600;
+
+console.log (ingresos >= objetivoIngresos)
+
+console.log (impuestos <= objetivoImpuestos)
+
+console.log (ganancias == objetivoGanancias)
