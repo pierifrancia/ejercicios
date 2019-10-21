7699333cca57d18066d0592adf016ddef787f49a
pierifrancia  <pieri.francia@gmail.com>
2019-10-21 20:26:42 -0300

    ej 34 y 36

--- ejercicios.js
+++ ejercicios.js
@@ -10,8 +10,38 @@ let objetivoGanancias = 600;
 
 let objetivoImpuestos = 600;
 
-console.log (ingresos >= objetivoIngresos)
+let mensajeIngresos = "Objetivo de ingresos";
+
+let mensajeImpuestos = "Objetivo de impuestos";
+
+let mensajeGanancias = "Objetivo de ganancias";
+
+console.log (mensajeIngresos);
+
+console.log (ingresos >= objetivoIngresos);
+
+console.log (mensajeImpuestos);
 
 console.log (impuestos <= objetivoImpuestos)
 
+console.log (mensajeGanancias);
+
 console.log (ganancias == objetivoGanancias)
+
+
+let numero1 = 10
+
+let numero2 = 20
+
+let mensajeMismoValor = "La variable numero1 y numero2 tienen el mismo valor:"
+
+let mensajeDiferenteValor = "La variables numero1 y numero2 son distintas"
+
+console.log (mensajeMismoValor);
+console.log (numero1 == numero2);
+
+console.log (mensajeDiferenteValor);
+console.log (numero1!=numero2);
+
+
+
